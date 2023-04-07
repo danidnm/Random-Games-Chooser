c10622015a774e6600adefe95c0303a120f64e51
Daniel Navarro <daniel@weareknitters.om>
2023-04-07 19:45:22 +0200

    Initial Commit

---
+++ Random Games Chooser/Random_Games_ChooserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Games_ChooserApp.swift
+//  Random Games Chooser
+//
+//  Created by Daniel Navarro on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Random_Games_ChooserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
